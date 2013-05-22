26c9223b505c08190995a65275da743d82b8a2dd
Greg Walden <gswalden@yahoo.com>
2013-05-22 00:02:33 -0400

    built user controller

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -4,17 +4,28 @@ class User extends CI_Controller {
 
 	public function index()
 	{
-		// $data['media'] = $this->instagram_api->getPopularMedia();
-		// $this->load->view('home', $data);
+		redirect('/');
 	}
 
 	public function code()
 	{
-		$get = $this->input->get('');
+		$get = $this->input->get('code');
 		
 		if ( ! empty($get))
 		{
+			$auth_response = $this->instagram_api->authorize($_GET['code']);
 
+			// Set up session variables containing some useful Instagram data
+			$this->session->set_userdata('instagram-token', $auth_response->access_token);
+			$this->session->set_userdata('instagram-username', $auth_response->user->username);
+			$this->session->set_userdata('instagram-profile-picture', $auth_response->user->profile_picture);
+			$this->session->set_userdata('instagram-user-id', $auth_response->user->id);
+			$this->session->set_userdata('instagram-full-name', $auth_response->user->full_name);
+
+			redirect('/');
 		}
+		else
+			redirect('/');
+
 	}
 }
