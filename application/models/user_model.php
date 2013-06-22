d10797dfaf3e9ae30618c0319a3781c2cd8e4162
Greg Walden <gswalden@yahoo.com>
2013-06-22 02:50:51 -0400

    comments!

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,6 +2,10 @@
 
 class User_model extends CI_Model {
 	
+	/**
+	 * [add_user description]
+	 * @param [type] $user [description]
+	 */
 	public function add_user($user)
 	{
 		$query = $this->db->get_where('users', ['id' => $user->id]);
@@ -19,11 +23,22 @@ class User_model extends CI_Model {
 		return ['bool' => FALSE, 'message' => 'Database error in ' . __METHOD__ . ': Number: ' . $this->db->_error_number() . '; Message: ' . $this->db->_error_message()];
 	}
 
+	/**
+	 * [get_user description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
 	public function get_user($id) 
 	{
 		return $this->db->get_where('users', ['id' => $id]);
 	}
 
+	/**
+	 * [get_users description]
+	 * @param  [type]  $access_level   [description]
+	 * @param  boolean $include_higher [description]
+	 * @return [type]                  [description]
+	 */
 	public function get_users($access_level = NULL, $include_higher = TRUE) 
 	{
 		if (isset($access_level))
@@ -37,6 +52,11 @@ class User_model extends CI_Model {
 		return $this->db->get('users');
 	}
 
+	/**
+	 * [delete_user description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
 	public function delete_user($id)
 	{
 		if ($this->db->delete('users', ['id' => $id]))
@@ -44,6 +64,12 @@ class User_model extends CI_Model {
 		return FALSE;
 	}
 
+	/**
+	 * [update_user description]
+	 * @param  [type] $id   [description]
+	 * @param  [type] $data [description]
+	 * @return [type]       [description]
+	 */
 	public function update_user($id, $data)
 	{
 		if ($this->db->update('users', $data, ['id' => $id]))
